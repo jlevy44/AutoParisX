7d3d9a7b699075d64099ad88291e858a8a0ce4a4
Joshua Levy <joshualevy44@berkeley.edu>
2023-10-27 18:06:56 -0700

    Update install_apx_dependencies.sh

--- scripts/install_apx_dependencies.sh
+++ scripts/install_apx_dependencies.sh
@@ -2,3 +2,5 @@
 pip install torch==1.10.2  torchvision torchaudio --upgrade --index-url https://download.pytorch.org/whl/cu102/
 pip install git+https://github.com/facebookresearch/detectron2.git
 pillow==9.5.0
+pip install "dask[distributed]"
+pip install cucim==23.8.0 cupy-cuda102==12.2.0
