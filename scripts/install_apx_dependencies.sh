280704427e80db0559a66b2d54fe6c00425eba06
Joshua Levy <joshualevy44@berkeley.edu>
2023-10-27 17:36:18 -0700

    update

--- scripts/install_apx_dependencies.sh
+++ scripts/install_apx_dependencies.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+pip install torch==1.10.2  torchvision torchaudio --upgrade --index-url https://download.pytorch.org/whl/cu102/
 pip install git+https://github.com/facebookresearch/detectron2.git
+pillow==9.5.0
